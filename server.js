e83213620124474c4dca9c732d9a67e841e773e1
ppremsagar <30975270+ppremsagar@users.noreply.github.com>
2017-08-27 12:40:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-   res.send('article 1 resuested and will be served here');
+   res.send(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two',function(req,res){
-   res.send('article 2 resuested and will be served here');
+   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three',function(req,res){
-   res.send('article 3 resuested and will be served here');
+   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
